a985f9758822989f603aee470bc805c30d1da245
Philipp Schlehuber-Caissier <schlehuber@isir.upmc.fr>
2019-08-18 00:04:29 +0200

    ..

--- code/lqr_discrete_recursion.py
+++ code/lqr_discrete_recursion.py
@@ -8,7 +8,7 @@ A = np.matrix([[0.,1.],[-0.1,-0.5]])
 B = np.matrix([[0.],[0.2]])
 
 Ad = sp_linalg.expm(A*dt)
-Bd = Ad*B
+Bd = np.matrix( np.dot(np.dot(sp_linalg.inv(A), (Ad-np.identity(2))), B))
 
 # Control
 Q = np.identity(2)
@@ -40,26 +40,37 @@ lqr_DynProg.getK = lambda t: lqr_DynProg.K[np.flatnonzero(lqr_DynProg.t<=t)[0]]
 # Get a reference trajectory
 #xd = Bd.u -> u = pinv(Bd).xd
 xref = lambda t: np.matrix([[np.sin( omega*np.floor(t/dt)*dt)], [omega*np.cos( omega*np.floor(t/dt)*dt)]])
-xdref = lambda t: np.matrix([[omega*np.cos( omega*np.floor(t/dt)*dt)], [-omega**2*np.sin( omega**2*np.floor(t/dt)*dt)]])
-uref = lambda t: np.matrix(sp_linalg.lstsq(Bd, xdref(t))[0])
+xdref = lambda t: np.matrix([[omega*np.cos( omega*np.floor(t/dt)*dt)], [-omega**2*np.sin( omega*np.floor(t/dt)*dt)]])
+# xd = A.x + B.u
+# u = inv(B).(xd-A.x)
+uref = lambda t: np.matrix(sp_linalg.lstsq(B, xdref(t)-A*xref(t))[0])
 
 # Simulate
 # Discrete
-x0 = xref(0.) + np.array([[0.5],[-0.2]])
+x0 = xref(0.) + np.array([[0.5],[-0.2]])*0.5
 X = np.matrix(np.zeros((2,Nsteps)))
 X[:,[0]] = x0
 for i in range(1,Nsteps):
     X[:,[i]] = Ad*X[:,[i-1]] + Bd*(uref(lqr_DynProg.t[i-1]) + lqr_DynProg.K[i-1]*(X[:,[i-1]]-xref(lqr_DynProg.t[i-1])))
-XREF = np.array([np.array(xref(at)) for at in lqr_DynProg.t])
+XREF = np.hstack([np.array(xref(at)) for at in lqr_DynProg.t])
+X = np.array(X)
 # Continuous
 f = lambda x,t: np.array((A*x.reshape((2,1)) + B*(uref(t) + lqr_DynProg.getK(t)*(x.reshape((2,1)) - xref(t))))).reshape((-1,))
-Xcont = sp_int.odeint(f, np.array(x0).reshape((-1,)), np.linspace(lqr_DynProg.t[0], lqr_DynProg.t[-1], 10*Nsteps))
+tCont = np.linspace(lqr_DynProg.t[0], lqr_DynProg.t[-1], 10*Nsteps)
+Xcont = sp_int.odeint(f, np.array(x0).reshape((-1,)), tCont).T
 
 
 ff,aa = plt.subplots(1,1)
 aa.plot(Xcont[0,:], Xcont[1,:], '--b')
 aa.plot(XREF[0,:], XREF[1,:], '.b')
 aa.plot(X[0,:], X[1,:], '.r')
+
+ff,aa = plt.subplots(2,1)
+for i in range(2):
+    aa[i].plot(tCont, Xcont[i,:], '-b')
+    aa[i].plot(lqr_DynProg.t, XREF[i,:], '-g')
+    aa[i].plot(lqr_DynProg.t, X[i,:], '-r')
+
 
 plt.show()
 
