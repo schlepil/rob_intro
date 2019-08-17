fad26e50150bdcd1a9ef82946b3efa8593d2d88a
Philipp Schlehuber-Caissier <schlehuber@isir.upmc.fr>
2019-08-17 11:44:20 +0200

    ..

--- code/lqr_discrete_recursion.py
+++ code/lqr_discrete_recursion.py
@@ -2,6 +2,8 @@ import numpy as np
 import scipy as sp
 from scipy import linalg as sp_linalg
 
+from matplotlib import pyplot as plt
+
 dt = 0.05
 
 # Define matrices
@@ -9,7 +11,7 @@ A = np.matrix([[0.,1.],[-0.1,-0.5]])
 B = np.matrix([[0.],[0.2]])
 
 Ad = sp_linalg.expm(A*dt)
-Bd = sp_linalg.expm(B*dt)
+Bd = Ad*B
 
 # Control
 Q = np.identity(2)
@@ -39,15 +41,24 @@ lqr_DynProg.P = reversed(lqr_DynProg.P)
 
 # Get a reference trajectory
 #xd = Bd.u -> u = pinv(Bd).xd
-xref = lambda t: np.sin( np.floor(t/dt)*dt )
-xdref = lambda t: np.cos( np.floor(t/dt)*dt )
+xref = lambda t: np.array([[np.sin( np.floor(t/dt)*dt)], [np.cos( np.floor(t/dt)*dt)]])
+xdref = lambda t: np.array([[np.cos( np.floor(t/dt)*dt)], [-np.sin( np.floor(t/dt)*dt)]])
 uref = lambda t: sp_linalg.lstsq(Bd, xdref(t))
 
 # Simulate
 x0 = xref(0.) + np.array([[0.5],[-0.2]])
+X = np.zeros((2,Nsteps))
+X[:,[0]] = x0
+for i in range(1,Nsteps):
+    X[:,[i]] = Ad*X[:,[i-1]] + Bd*(uref(lqr_DynProg.t[i-1]) + lqr_DynProg.K[i-1]*(X[:,[i-1]]-xref(lqr_DynProg.t[i-1])))
+XREF = xref(lqr_DynProg.t)
 
 
+ff,aa = plt.subplots(1,1)
+aa.plot(XREF[0,:], XREF[1,:], '.b')
+aa.plot(X[0,:], X[1,:], '.r')
 
+plt.show()
 
 
 
